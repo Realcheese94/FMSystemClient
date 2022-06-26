bdf55d5aafbe80ef5f41c7506074e2adafef7c84
Realcheese94 <35086477+Realcheese94@users.noreply.github.com>
2022-06-26 13:47:39 +0900

    Smith
    spirng boot actuator, admin register

---
+++ src/main/java/com/practice/demo/FmSystemClientApplication.java
@@ -0,0 +1,13 @@
+package com.practice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmSystemClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmSystemClientApplication.class, args);
+	}
+
+}
