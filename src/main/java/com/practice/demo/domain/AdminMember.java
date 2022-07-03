3f172053716f3459757400b0392edc78691cf342
Realcheese94 <35086477+Realcheese94@users.noreply.github.com>
2022-07-03 17:19:01 +0900

    jpa, h2 setting

--- src/main/java/com/practice/demo/domain/AdminMember.java
+++ src/main/java/com/practice/demo/domain/AdminMember.java
@@ -1,8 +1,19 @@
 package com.practice.demo.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class AdminMember {
 
+	@Id
+	@GeneratedValue(strategy =  GenerationType.AUTO)
 	private Long id;
+	@Column
 	private String adminId;
+	@Column
 	private String adminPw;
 }
